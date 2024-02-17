f6f3f8c514de5422dcb0ee0d1955cc29487b45de
AkRao47 <159660084+AkRao47@users.noreply.github.com>
2024-02-18 04:06:25 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -1,8 +1,11 @@
+
+
 import glob
 from pathlib import Path
 from main.utils import load_plugins
 import logging
-from . import bot
+from main import bot
+from flask import Flask
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
@@ -15,11 +18,14 @@ for name in files:
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
 
-#Don't be a thief 
+# Create a Flask app
+app = Flask(__name__)
+
+# Don't be a thief
 print("Successfully deployed!")
 print("By MaheshChauhan • DroneBots")
 
 if __name__ == "__main__":
-    bot.run_until_disconnected()
-
-
+    bot.start()  # Assuming 'start' is the method to start the bot in your 'bot' instance
+    # Run the Flask app on port 5000
+    app.run(host='0.0.0.0', port=5000)
